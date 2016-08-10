f27f2496167d536d366daa2e57be07f5ad4462c4
Wusiwug0 <palagiris.0201@gmail.com>
2016-08-10 12:50:33 -0400

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Helloooo NewYork Times!!!!');
+  res.send('Helloooo Everyone!!!! ');
 });
 
 app.listen(8080, function () {
